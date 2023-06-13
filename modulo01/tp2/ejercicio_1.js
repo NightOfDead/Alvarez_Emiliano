7c7721cc94fbc055dc3776b8aca269e793550feb
Alvarez Emiliano Valentín <136206038+NightOfDead@users.noreply.github.com>
2023-06-12 21:24:33 -0300

    Delete modulo01/tp2 directory

--- modulo01/tp2/ejercicio_1.js
+++
@@ -1,2 +0,0 @@
-document.writeln("Hola Mundo!");
-// Imprime la frase "Hola Mundo!"
--- modulo01/tp2/ejercicio_9.js
+++
@@ -1,3 +0,0 @@
-let Vocales = ["A", "E", " I", "O", "U"];
-
-document.write(`${Vocales[0]} <br/> <br/> ${Vocales[1]} <br/> <br/> ${Vocales[2]} <br/> <br/> ${Vocales[3]} <br/> <br/> ${Vocales[4]} <br/> <br/>` );
