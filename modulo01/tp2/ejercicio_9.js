7e132b35212e06d97fabdadcfd05de01b787879b
Alvarez Emiliano Valentín <136206038+NightOfDead@users.noreply.github.com>
2023-06-12 21:23:01 -0300

    Add files via upload

---
+++ modulo01/tp2/ejercicio_9.js
@@ -0,0 +1,3 @@
+let Vocales = ["A", "E", " I", "O", "U"];
+
+document.write(`${Vocales[0]} <br/> <br/> ${Vocales[1]} <br/> <br/> ${Vocales[2]} <br/> <br/> ${Vocales[3]} <br/> <br/> ${Vocales[4]} <br/> <br/>` );
